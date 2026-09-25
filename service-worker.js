9dd77bfe4f2a276625a7251926ad3de6e452d5ac
Carlos Fernando Aleman Garcia <alemangcf@gmail.com>
2026-09-24 19:29:04 -0600

    Autocompletado de productos con catalogo precargado

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "compras-hogar-v2";
+const CACHE = "compras-hogar-v3";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
