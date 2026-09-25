b2f5476f95793114d2bd317e76d243f5fd8d9cb8
Carlos Fernando Aleman Garcia <alemangcf@gmail.com>
2026-09-24 19:21:41 -0600

    Bump cache a v2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "compras-hogar-v1";
+const CACHE = "compras-hogar-v2";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
